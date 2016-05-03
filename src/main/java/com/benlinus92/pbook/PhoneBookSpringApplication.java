fed6cdf3111fe762b92e10eeac0b976e8b4f71b9
father-gregor <benlinus92@gmail.com>
2016-05-03 21:16:36 +0300

    Add configuration file for JPA

--- src/main/java/com/benlinus92/pbook/PhoneBookSpringApplication.java
+++ src/main/java/com/benlinus92/pbook/PhoneBookSpringApplication.java
@@ -1,4 +1,4 @@
-package com.benlinus92.pbook.config;
+package com.benlinus92.pbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/benlinus92/pbook/PhoneBookSpringApplicationTests.java
+++ src/test/java/com/benlinus92/pbook/PhoneBookSpringApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import com.benlinus92.pbook.config.PhoneBookSpringApplication;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = PhoneBookSpringApplication.class)
 @WebAppConfiguration
