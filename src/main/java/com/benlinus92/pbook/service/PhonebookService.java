0c97729d655ce21d1ac866390602f39e8cdac367
father-gregor <benlinus92@gmail.com>
2016-05-17 21:18:14 +0300

    Add function for finding profiles

--- src/main/java/com/benlinus92/pbook/service/PhonebookService.java
+++ src/main/java/com/benlinus92/pbook/service/PhonebookService.java
@@ -1,11 +1,17 @@
 package com.benlinus92.pbook.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.benlinus92.pbook.domains.Profile;
+import com.benlinus92.pbook.domains.User;
 
 @Service("phonebookService")
 @Transactional
 public interface PhonebookService {
-	
-	
+	public boolean addNewUser(User user);
+	public User findUserByUsername(String login);
+	public List<Profile> getProfiles();
 }
--- src/main/java/com/benlinus92/pbook/service/PhonebookServiceImpl.java
+++ src/main/java/com/benlinus92/pbook/service/PhonebookServiceImpl.java
@@ -1,5 +1,39 @@
 package com.benlinus92.pbook.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.benlinus92.pbook.dao.PhonebookDao;
+import com.benlinus92.pbook.domains.Profile;
+import com.benlinus92.pbook.domains.User;
+
+@Service("phonebookService")
+@Transactional
 public class PhonebookServiceImpl implements PhonebookService {
 
+	@Autowired
+	public PhonebookDao dao;
+	@Override
+	public boolean addNewUser(User user) {
+		if(dao.findUserByUsername(user.getLogin()) == null) {
+			user.setProfiles(getProfiles());
+			dao.saveUser(user);
+			return true;
+		}
+		return false;
+	}
+	@Override
+	public User findUserByUsername(String login) {
+		return dao.findUserByUsername(login);
+	}
+	@Override
+	public List<Profile> getProfiles() {
+		List<Profile> auth = new ArrayList<>();
+		auth.add(dao.findProfile("USER"));
+		return auth;
+	}
 }
