33b607eb4a9091f39b4ec3032720b45d938d677a
father-gregor <benlinus92@gmail.com>
2016-05-17 21:12:36 +0300

    Add post function for register new user

--- src/main/resources/static/js/entry_service.js
+++ src/main/resources/static/js/entry_service.js
@@ -1,5 +1,5 @@
 
-app.factory("EntryService", function($http, $q) {
+app.factory("EntryService", function($http, $q, $location) {
 	return {
 		createEntry: function(entry) {
 			return $http.get("/create-entry", entry)
@@ -9,6 +9,17 @@ app.factory("EntryService", function($http, $q) {
 					},
 					function(err) {
 						console.log("Error while creating");
+						return $q.reject(err);
+					}
+			);
+		},
+		registerUser: function(user) {
+			return $http.post("/register-user", user)
+			.then(
+					function(response) {
+						return response.data;
+					},
+					function(err) {
 						return $q.reject(err);
 					}
 			);
--- src/main/resources/static/js/register_ctrl.js
+++ src/main/resources/static/js/register_ctrl.js
@@ -1,4 +1,44 @@
 
-app.controller("RegisterController", function($scope) {
-	
+app.controller("RegisterController", function($scope, $location, EntryService) {
+	$scope.user = {
+		username: null,
+		email: null,
+		password: null,
+		passConf: null,
+		realName: null,
+		realFather: null,
+		realSurname: null
+	};
+	$scope.send = function() {
+		if($scope.form.$valid && $scope.isPassMatch()) {
+			var endUser = {
+				login: $scope.user.username,
+				password: $scope.user.password,
+				fio: $scope.user.realName + " " + $scope.user.realFather + " " + $scope.user.realSurname
+			};
+			EntryService.registerUser(endUser)
+			.then(function(data) {
+				console.log(data);
+				$location.path(data);
+			}, function(err) {
+				console.log("Error");
+				console.log(err);
+			});
+		}
+		//$scope.submitted = true;
+		console.log($scope.user);
+	};
+	$scope.hasFormError = function() {
+		return $scope.form.$invalid & $scope.submitted;
+	}
+	$scope.hasError = function(field, valid) {
+		return $scope.form[field].$invalid && $scope.form.$submitted;
+	}
+	$scope.isPassMatch = function() {
+		return $scope.user.password === $scope.user.passConf;
+	}
+	$scope.getPath = function() {
+		//$location.path(path);
+		console.log("Work");
+	}
 });
