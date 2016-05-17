7b3a2324948d5db5b1b4bdee11e8156c28d28bcf
father-gregor <benlinus92@gmail.com>
2016-05-17 21:14:55 +0300

    Add function for finding profile with type user

--- src/main/java/com/benlinus92/pbook/dao/PhoneBookDaoImpl.java
+++ src/main/java/com/benlinus92/pbook/dao/PhoneBookDaoImpl.java
@@ -1,11 +1,46 @@
 package com.benlinus92.pbook.dao;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.benlinus92.pbook.domains.Profile;
+import com.benlinus92.pbook.domains.User;
+
+@Repository
+@Transactional
 public class PhoneBookDaoImpl implements PhonebookDao {
 	@PersistenceContext
 	EntityManager em;
 	
-
+	@Override
+	public void saveUser(User user) {
+		em.persist(user);
+	}
+	@Override
+	public User findUserByUsername(String login) {
+		User user = null;
+		try {
+			Query query = em.createQuery("SELECT r FROM User r WHERE r.login=:login", User.class);
+			query.setParameter("login", login);
+			user = (User)query.getSingleResult();
+		} catch (NoResultException e) {		}
+		return user;
+	}
+	@Override
+	public Profile findProfile(String type) {
+		Profile profile = null;
+		try {
+			Query query = em.createQuery("SELECT r FROM Profile r WHERE r.type=:type", Profile.class);
+			query.setParameter("type", type);
+			profile = (Profile)query.getSingleResult();
+		} catch(NoResultException e) {		}
+		return profile;
+	}
 }
--- src/main/java/com/benlinus92/pbook/dao/PhonebookDao.java
+++ src/main/java/com/benlinus92/pbook/dao/PhonebookDao.java
@@ -1,5 +1,12 @@
 package com.benlinus92.pbook.dao;
 
-public interface PhonebookDao {
+import java.util.List;
 
+import com.benlinus92.pbook.domains.Profile;
+import com.benlinus92.pbook.domains.User;
+
+public interface PhonebookDao {
+	public void saveUser(User user);
+	public User findUserByUsername(String login);
+	public Profile findProfile(String type);
 }
